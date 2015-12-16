7770552b3d5e8e0a6a56bb40214b0337120629e7
Jonathan Davis <jonathanherscheldavis@gmail.com>
2015-12-16 08:25:02 -0500

    added testing of strings and numbers

--- cheatsheet.js
+++ cheatsheet.js
@@ -2,15 +2,34 @@ var x = 5;
 var y = 10;
 var result = x + y;
 console.log(result);
-//basic adding variables
+//basic adding number data types
 
 var alert1 = "This";
 var alert2 = "alert!";
 alert(alert1 +  " " + "is" + " " + "an" + " " + alert2);
-//adding variables to make an alert
+//adding strings to make an alert
 
 function change() {
 	document.getElementById("surprise").innerHTML = "BOO! Didn't see THAT coming";
 }
 //a change function
+
+var superhero = ["Iron Man", "Captain America", "Thor"];
+//an array, to be used in object below
+
+var movies = {octane: "Die Hard", comedy: "Anchorman", superhero: superhero};
+console.log(movies.octane);
+console.log(movies.comedy);
+console.log(superhero[1]);
+//an object
+
+"this string" === "this string"
+console.log("this string" === "that string");
+//string testing
+
+console.log((15-6) === (9))
+//number testing
+
+
+
 
