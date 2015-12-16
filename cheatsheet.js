7d21aa3e877e3ed6d513b1d49d502c1498e4d538
Jonathan Davis <jonathanherscheldavis@gmail.com>
2015-12-16 07:34:43 -0500

    added change function

--- cheatsheet.js
+++ cheatsheet.js
@@ -2,7 +2,15 @@ var x = 5;
 var y = 10;
 var result = x + y;
 console.log(result);
+//basic adding variables
 
 var alert1 = "This";
 var alert2 = "alert!";
 alert(alert1 +  " " + "is" + " " + "an" + " " + alert2);
+//adding variables to make an alert
+
+function change() {
+	document.getElementById("surprise").innerHTML = "BOO! Didn't see THAT coming";
+}
+//a change function
+
